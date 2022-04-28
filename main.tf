c3592d78bfa7c4fb7ff49a21ef470a1bbb8ea92d
José Carvalho <48421530+JoseCarvalho1026@users.noreply.github.com>
2022-04-28 12:01:11 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,28 +1,16 @@
 # aws_eip.eip0:
 resource "aws_eip" "eip0" {
-    network_border_group = "us-east-1"
-    tags                 = {}
-    vpc                  = true
+  network_border_group = "us-east-1"
+  tags                 = {}
+  vpc                  = true
 
-    timeouts {}
-}
-
-# aws_vpc.vpc0:
-resource "aws_vpc" "vpc0" {
-    assign_generated_ipv6_cidr_block = false
-    cidr_block                       = "172.31.0.0/16"
-    enable_classiclink               = false
-    enable_classiclink_dns_support   = false
-    enable_dns_hostnames             = true
-    enable_dns_support               = true
-    instance_tenancy                 = "default"
-    tags                             = {}
+  timeouts {}
 }
 
 # aws_security_group.sg0:
 resource "aws_security_group" "sg0" {
-    description = "Grupo de seguranca para o exercicio um"
-    egress      = [
+    description = "JoseCarvalho - Security Group"
+        egress      = [
         {
             cidr_blocks      = [
                 "0.0.0.0/0",
@@ -36,39 +24,14 @@ resource "aws_security_group" "sg0" {
             self             = false
             to_port          = 0
         },
-    ]
-    ingress     = [
-        {
+  ]
+    
+        ingress     = [
+    {
             cidr_blocks      = [
                 "0.0.0.0/0",
             ]
-            description      = "Permitir todo o ICMP"
-            from_port        = -1
-            ipv6_cidr_blocks = []
-            prefix_list_ids  = []
-            protocol         = "icmp"
-            security_groups  = []
-            self             = false
-            to_port          = -1
-        },
-        {
-            cidr_blocks      = [
-                "78.29.147.32/32",
-            ]
-            description      = "Permitir UDP"
-            from_port        = 80
-            ipv6_cidr_blocks = []
-            prefix_list_ids  = []
-            protocol         = "udp"
-            security_groups  = []
-            self             = false
-            to_port          = 80
-        },
-        {
-            cidr_blocks      = [
-                "78.29.147.32/32",
-            ]
-            description      = "Permitir o HTTPs vindo do meu IP"
+            description      = ""
             from_port        = 443
             ipv6_cidr_blocks = []
             prefix_list_ids  = []
@@ -78,12 +41,38 @@ resource "aws_security_group" "sg0" {
             to_port          = 443
         },
         {
-            cidr_blocks      = []
-            description      = "Permitir o HTTP vindo de IPv6"
-            from_port        = 80
-            ipv6_cidr_blocks = [
-                "::/0",
+            cidr_blocks      = [
+                "78.29.147.32/32",
             ]
+            description      = ""
+            from_port        = 0
+            ipv6_cidr_blocks = []
+            prefix_list_ids  = []
+            protocol         = "-1"
+            security_groups  = []
+            self             = false
+            to_port          = 0
+        },
+        {
+            cidr_blocks      = [
+                "78.29.147.32/32",
+            ]
+            description      = ""
+            from_port        = 22
+            ipv6_cidr_blocks = []
+            prefix_list_ids  = []
+            protocol         = "tcp"
+            security_groups  = []
+            self             = false
+            to_port          = 22
+        },
+        {
+            cidr_blocks      = [
+                "78.29.147.32/32",
+            ]
+            description      = ""
+            from_port        = 80
+            ipv6_cidr_blocks = []
             prefix_list_ids  = []
             protocol         = "tcp"
             security_groups  = []
@@ -91,34 +80,27 @@ resource "aws_security_group" "sg0" {
             to_port          = 80
         },
     ]
-    name        = "grsi-security-group"
+    name        = "JoseCarvalho"
     tags        = {}
-    vpc_id      = "vpc-09ab79262e9b1a9ee"
 
     timeouts {}
 }
 
-# aws_instance.instance0:
-resource "aws_instance" "GRSI" {
-    ami                          = "ami-0f9fc25dd2506cf6d"
-    availability_zone            = "us-east-1b"
+# aws_instance.instance:
+resource "aws_instance" "instance" {
+    ami                          = "ami-04505e74c0741db8d"
+    availability_zone            = "us-east-1f"
     instance_type                = "t2.micro"
     key_name                     = "vockey"
     security_groups              = [
         aws_security_group.sg0.name,
     ]
     vpc_security_group_ids       = [
-        aws_vpc.vpc0.id,
+        aws_security_group.sg0.id,
     ]
-    ebs_block_device {
-        delete_on_termination = true
-        device_name           = "/dev/sdb"
-        volume_size           = 8
-        volume_type           = "gp3"
-    }
     root_block_device {
         delete_on_termination = true
-        volume_size           = 8
+        volume_size           = 30
         volume_type           = "gp2"
     }
      
@@ -127,5 +109,5 @@ resource "aws_instance" "GRSI" {
 # aws_eip_association.eip0_assoc:
 resource "aws_eip_association" "eip0_assoc" {
   allocation_id        = aws_eip.eip0.id
-instance_id          = aws_eip.eip0.id
+instance_id          = aws_instance.instance.id
 }
